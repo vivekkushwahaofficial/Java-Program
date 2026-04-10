534b4a83d18c6b0f51ec96226020756094b4eb88
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-04-10 22:32:59 +0530

    Two Pointer

---
+++ JAVA_DSA/array/twoSum1.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+class twoSum1{
+  public static int[] twoSum(int[] numbers, int target){
+    int i = 0;
+    int j = numbers.length -1;
+
+    while(i<j){
+    int sum = numbers[i] + numbers[j];
+    if(sum == target){
+      return new int[]{i+1, j+1};
+    } else if(sum > target){
+      j--;
+    } else {
+      i++;
+    }
+  }
+    return new int[] {-1, -1};
+  }
+  public static void main(String[] args) {
+      int[] numbers = {-1,0};
+      int target = -1;
+      int[] result = twoSum(numbers, target);
+      System.out.println(Arrays.toString(result));
+  }
+}
