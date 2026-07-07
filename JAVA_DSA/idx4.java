b710ddf5cc2fc2a2d4b74a21ea475fd2bbe74c0f
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-07-07 23:25:03 +0530

    update code of idx4

--- JAVA_DSA/idx4.java
+++ JAVA_DSA/idx4.java
@@ -1,6 +1,5 @@
 
 // public class idx4 {
-
 //     public static void main(String[] args) {
 //         int[] arr = {4, 3, 8, 7, 5};
 //         for (int i = 0; i < arr.length; i++) {
@@ -20,15 +19,12 @@
 //         // System.err.println(arr[4]);
 //     }
 // }
-
-
-
 public class idx4 {
 
     public static void main(String[] args) {
         int[] arr = {4, 3, 8, 7, 5};
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[i];
@@ -39,7 +35,7 @@ public class idx4 {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        // System.err.println();
-        // System.out.println(arr[arr.length - 4]);
+        System.err.println();
+        System.out.println(arr[arr.length - 4]);
     }
 }
