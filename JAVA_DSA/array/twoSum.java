9c614f900ffb4a52a82723a5c3ac334874f15bcf
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-07-07 23:11:24 +0530

    Improve TwoSum solutions and refactor code

--- JAVA_DSA/array/twoSum.java
+++ JAVA_DSA/array/twoSum.java
@@ -1,24 +1,26 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class TwoSum {
+public class twoSum {
 
-  // Return the Value
-    public static int[] twoSumValues(int[] arr, int target) {
-        
+    public static int[] twoSum1(int[] arr) {
+        int[] arr1 = new int[2];
+        int target = 9;
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] + arr[j] == target) {
-                   return new int[]{arr[i], arr[j]};
+                    arr1[0] = arr[i];
+                    arr1[1] = arr[j];
+                    return arr1;
 
                 }
             }
         }
-        return new int[]{-1, -1};
+        return arr1;
     }
-//   Return the Indices
-    public static int[] twoSumIndicesBruteForce(int[] arr, int target) {
+
+    public static int[] twoSum2(int[] arr, int target) {
         int[] arr2 = new int[2];
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -29,10 +31,10 @@ public class TwoSum {
                 }
             }
         }
-        return new int[]{-1, -1};
+        return arr2;
     }
 
-    public static int[] twoSumIndicesHashMap(int[] arr, int target) {
+    public static int[] twoSum3(int[] arr, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
@@ -42,7 +44,6 @@ public class TwoSum {
             if (map.containsKey(need)) {
                 return new int[]{map.get(need), i};
             }
-            map.put(current, i);
         }
         return new int[]{-1, -1};
     }
@@ -51,14 +52,12 @@ public class TwoSum {
         int target = 9;
         // int arr1 = new int[2];
         int[] arr = {2, 4, 6, 7, 4};
-        int[] result1 = twoSumValues(arr, target);
+        int[] result = twoSum1(arr);
+        System.out.println(Arrays.toString(result));
+        System.out.println(result[0] + result[1]);
+        int[] result1 = twoSum2(arr, target);
         System.out.println(Arrays.toString(result1));
-        System.out.println(result1[0] + result1[1]);
-        int[] result2 = twoSumIndicesBruteForce(arr, target);
-        System.out.println(Arrays.toString(result2));
         // System.out.println("idx" + "[" + result[0] + "]" + "," + "idx" + "[" + result[1] + "]");
-        // System.out.println("idx[" + result[0] + "], " + "idx[" + result[1] + "]");
-          int[] result3 = twoSumIndicesHashMap(arr, target);
-        System.out.println(Arrays.toString(result3));
+        System.out.println("idx[" + result[0] + "], " + "idx[" + result[1] + "]");
     }
 }
